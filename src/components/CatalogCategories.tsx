b67de98e6fbf2c423f0587e9d46c19b3bb5e7077
Sanchir <emgushovs@mail.ru>
2023-04-02 23:30:36 +0300

    переписал полностью баÐд, теперь она прÐавильная, сделал меню вылетающее при наведении и фон при наведении

--- src/components/CatalogCategories.tsx
+++ src/components/CatalogCategories.tsx
@@ -1,14 +1,37 @@
 import React from "react";
 import { api } from "~/utils/api";
-import { GetStaticProps } from "next";
 
 const CatalogCategories = () => {
   const { data } = api.category.getAll.useQuery();
+
+  const [subCatalog, setSubCatalog] = React.useState<number | null>();
+
   return (
     <div>
-      {data?.map((category) => (
-        <div key={category.id}>{category.title}</div>
-      ))}
+      <div className="relative z-20 m-2 flex gap-4 rounded-lg bg-white">
+        <div className="">
+          {data?.map((category) => (
+            <div
+              onMouseEnter={() => setSubCatalog(category.id)}
+              key={category.id}
+            >
+              {category.title}
+            </div>
+          ))}
+        </div>
+        {subCatalog && (
+          <div className="flex-1">
+            {data
+              ?.find((category) => category.id === subCatalog)
+              ?.subCategories.map((subCategory) => (
+                <div key={subCategory.title}>{subCategory.title}</div>
+              ))}
+          </div>
+        )}
+      </div>
+      {subCatalog && (
+        <div className="absolute right-0 top-0 h-screen w-full bg-neutral-600 opacity-25"></div>
+      )}
     </div>
   );
 };
--- src/server/api/routers/category.ts
+++ src/server/api/routers/category.ts
@@ -4,6 +4,9 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const categoryRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.category.findMany({ include: { subCategories: true } });
+    return ctx.prisma.category.findMany({
+      where: { parent: null },
+      include: { subCategories: true },
+    });
   }),
 });
